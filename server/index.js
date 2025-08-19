d10e873c30fa916d5a21b83b49ae83e4dfe5a62b
Aditya Shivhare <shivhareaditya2907@gmail.com>
2025-08-20 00:39:29 +0530

    Project Uploaded

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-server.listen(1337);
+const PORT = process.env.PORT || 8000;
 
-app.use(express.static(__dirname + "/../client"));
+app.use(express.static(path.join(__dirname, "../client")));
+
+const indexHtmlPath = path.join(__dirname, "../index.html");
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../client/index.html");
+  res.sendFile(indexHtmlPath);
 });
+
+app.get("*", (req, res) => {
+  res.sendFile(indexHtmlPath);
+});
+
+server.listen(PORT);
